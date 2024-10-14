caecbe9988dcba70b137d5f916538e310fa71267
Maxmutov Nuriddin <mahmudovnuriddin35@gmail.com>
2024-10-14 12:17:40 +0500

    update app.js

--- app.js
+++ app.js
@@ -3,19 +3,22 @@ const path = require('path');
 const indexRouter = require('./routes/index');
 
 const app = express();
-const PORT = 3000;
 
-// Serve static files from the "public" directory
+// Используем порт из переменной окружения или 3000 для локальной разработки
+const PORT = process.env.PORT || 3000;
+
+// Обслуживаем статические файлы из директории "public"
 app.use(express.static(path.join(__dirname, 'public')));
 
-// Use the router for handling routes
+// Используем маршрутизацию из файла routes/index.js
 app.use('/', indexRouter);
 
-// Catch-all route for handling 404 errors
+// Обработка 404 ошибок
 app.use((req, res, next) => {
-    res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
-  });
+  res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
+});
 
+// Запуск сервера
 app.listen(PORT, () => {
   console.log(`Server running at http://localhost:${PORT}/`);
 });
